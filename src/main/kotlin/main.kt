ceb9dfea32955f410deb267bf41bae8ca2fa18b4
Slava Tumanov <vn.tumanov@me.com>
2020-09-26 23:03:34 +0300

    del BigDecimal -> changed to Long. fun's add return value

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,16 @@
 package ru.netology
 
-import java.math.BigDecimal
-
 fun main() {
     val cardType = "MasterCard"
-    val monthTransfer: BigDecimal = BigDecimal.valueOf(320_000_000)
-    val transfer: BigDecimal = BigDecimal.valueOf(200_000)
+    val monthTransfer = 320_000_000L
+    val transfer = 2_000_000L
     commissionAmount(cardType, monthTransfer, transfer)
 }
 
 private fun commissionAmount(
-    cardType: String = "VK Pay",
-    monthTransfer: BigDecimal = BigDecimal.valueOf(0),
-    transfer: BigDecimal
+        cardType: String = "VK Pay",
+        monthTransfer: Long = 0L,
+        transfer: Long
 ) {
     when (cardType) {
         "MasterCard", "Maestro" -> {
@@ -25,26 +23,24 @@ private fun commissionAmount(
     }
 }
 
-private fun calForVisaMirCard(transfer: BigDecimal) {
-    val commission = transfer.times(0.0075.toBigDecimal())
-    if (commission > (3500).toBigDecimal()) {
+private fun calForVisaMirCard(transfer: Long): Long {
+    val commission = transfer * 0.0075
+    return if (commission > 3500) {
         println("Комиссия: $commission коп")
-        val totalTransfer = (transfer.plus(commission)).div(100.toBigDecimal())
-        println("Итого с комиссией: $totalTransfer руб")
+        commission.toLong()
     } else {
         println("Комиссия: 3500 коп")
-        val totalTransfer = (transfer.plus(3500.toBigDecimal())).div(100.toBigDecimal())
-        println("Итого с комиссией: $totalTransfer руб")
+        3500
     }
 }
 
-private fun calForMasterMaestroCard(monthTransfer: BigDecimal, transfer: BigDecimal) {
-    if (monthTransfer.add(transfer) <= 7_500_000.toBigDecimal()) {
+private fun calForMasterMaestroCard(monthTransfer: Long, transfer: Long): Long {
+    return if (monthTransfer + transfer <= 7_500_000) {
         println("Комиссия не взимается")
+        0
     } else {
-        val commission = transfer.times(0.006.toBigDecimal()).plus(2000.toBigDecimal())
+        val commission = transfer * 0.006 + 2000
         println("Комиссия: $commission коп")
-        val totalTransfer = (transfer.plus(commission)).div(100.toBigDecimal())
-        println("Итого с комиссией: $totalTransfer руб")
+        commission.toLong()
     }
 }
